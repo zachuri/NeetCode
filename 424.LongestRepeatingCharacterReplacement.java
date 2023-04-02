b2f9dd2335d7e1fcc947e5da81ad16e508437d09
zachuri <punsalang.zachary@gmail.com>
2023-04-01 22:56:58 -0700

    424: added better comments

---
+++ 424.LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    // Set -> doesn't contain duplicates
+    Set<Character> set = new HashSet<>();
+
+    int left = 0;
+    int right = 0;
+    int n = s.length();
+
+    int maxLen = 0;
+
+    while (right < n) {
+      // at current right idx -> check if contains
+      if (!set.contains(s.charAt(right))) {
+
+        // add to char to set with right idx
+        set.add(s.charAt(right));
+
+        // add maxLen -> maxLen, calculate maxLen of right and left idx 
+        maxLen = Math.max(maxLen, right - left + 1);
+
+        right++;
+      }
+      else {
+        // remove character from set with left idx 
+        set.remove(s.charAt(left));
+
+        left++;
+      }
+    } 
+
+    return maxLen;
+  }
+}
