3d22ee9bad1f11f50aca6bb843458f51e31a1126
zachuri <punsalang.zachary@gmail.com>
2023-04-04 02:53:45 -0700

    125. Valid Palindrome

---
+++ 125.ValidPalindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+  public boolean isPalindrome(String s) {
+    s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+    
+    int i = 0;
+    int j = s.length() - 1;
+    
+    while (i < j) {
+        if (s.charAt(i) != s.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+  }
+}
