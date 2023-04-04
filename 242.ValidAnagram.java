cd0c5b746bc82493e646c17d7f450e5afaa28d5e
zachuri <punsalang.zachary@gmail.com>
2023-04-04 02:59:44 -0700

    242. Valid Anagram

---
+++ 242.ValidAnagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] count = new int[128];
+        
+        for (int i = 0; i < s.length(); ++i) {
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
+        }
+        
+        for (int i = 0; i < count.length; ++i) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
